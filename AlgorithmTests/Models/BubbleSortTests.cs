2a2ad8b24d1c5d6f0dde91f7fbeb24875b2a4189
Aleksey Zhalnin <zhalninreg@me.com>
2023-08-18 09:57:33 +0300

    Added MSTest

---
+++ AlgorithmTests/Models/BubbleSortTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Models.Tests
+{
+    [TestClass()]
+    public class BubbleSortTests
+    {
+        [TestMethod()]
+        public void SortTest()
+        {
+            //Arrange
+            BubbleSort<int> bubble = new();
+            List<int> items = new();
+            bubble.FillRandom(10, items);
+
+            //Act
+            items.Sort();
+            bubble.Sort();
+
+            //Assert
+            for (int i = 0; i < items.Count; i++)
+            {
+                Assert.AreEqual(items[i], bubble.Items[i]);
+            }
+        }
+    }
+}
