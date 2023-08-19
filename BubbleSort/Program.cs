75a8d03ebfa795d8173be8d2700b188e86e5e4f4
Aleksey Zhalnin <zhalninreg@me.com>
2023-08-19 10:08:51 +0300

    Updated bubble sorting

--- BubbleSort/Program.cs
+++ BubbleSort/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Algorithm.Extensions;
+using Algorithm.Models;
+
 Console.WriteLine("Hello, buble sorting!");
+
+
+BubbleSort<int> bubble = new();
+bubble.FillRandom(10);
+bubble.Sort();
