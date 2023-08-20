d753ff69f4f0ee1bdfc8e899d112aca3e351a6b0
Aleksey Zhalnin <zhalninreg@me.com>
2023-08-20 18:57:10 +0300

    Added insert sort type and unify unit tests into one class

---
+++ Algorithm/Models/InsertSort.cs
@@ -0,0 +1,22 @@
+﻿namespace Algorithm.Models;
+
+public class InsertSort<T> : AlgorithmBase<T> 
+    where T : IComparable<T>
+{
+    protected override void MakeSort()
+    {
+        for (int i = 1; i < Items.Count; i++)
+        {
+            var temp = Items[i];
+            var j = i;
+            while(j > 0 && temp.CompareTo(Items[j - 1]) < 0)
+            {
+                Items[j] = Items[j - 1];
+                j--;
+                SwapCount++;
+                ComparisonCount++;
+            }
+            Items[j] = temp;
+        }
+    }
+}
---
+++ AlgorithmTests/Models/SortingTests.cs
@@ -0,0 +1,72 @@
+﻿using Algorithm.Extensions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithm.Models.Tests;
+
+[TestClass()]
+public class SortingTests
+{
+    readonly List<int> items = new();
+    AlgorithmBase<int> sortType = new();
+
+    [TestInitialize]
+    public void Init()
+    {
+        items.Clear();
+    }
+
+    [TestMethod()]
+    public void BubbleSortTest()
+    {
+        //Arrange
+        sortType = new BubbleSort<int>();
+        sortType.FillRandom(10, items);
+
+        //Act
+        items.Sort();
+        var timer = sortType.Sort();
+        Console.WriteLine(timer);
+
+        //Assert
+        for (int i = 0; i < items.Count; i++)
+        {
+            Assert.AreEqual(items[i], sortType.Items[i]);
+        }
+    }
+
+    [TestMethod()]
+    public void CocktailSortTest()
+    {
+        //Arrange
+        sortType = new CocktailSort<int>();
+        sortType.FillRandom(10, items);
+
+        //Act
+        items.Sort();
+        var timer = sortType.Sort();
+        Console.WriteLine(timer);
+        //Assert
+        for (int i = 0; i < items.Count; i++)
+        {
+            Assert.AreEqual(items[i], sortType.Items[i]);
+        }
+    }
+
+    [TestMethod()]
+    public void InsertSortTest()
+    {
+        //Arrange
+        sortType = new InsertSort<int>();
+        sortType.FillRandom(10, items);
+
+        //Act
+        items.Sort();
+        var timer = sortType.Sort();
+        Console.WriteLine(timer);
+        //Assert
+        for (int i = 0; i < items.Count; i++)
+        {
+            Assert.AreEqual(items[i], sortType.Items[i]);
+        }
+    }
+}
