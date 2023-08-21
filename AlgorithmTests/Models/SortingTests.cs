78cc79dce58fb28bf3090920eac0f894b2561a9a
Aleksey Zhalnin <zhalninreg@me.com>
2023-08-21 11:17:08 +0300

    Added shell sorting

--- AlgorithmTests/Models/SortingTests.cs
+++ AlgorithmTests/Models/SortingTests.cs
@@ -69,4 +69,22 @@ public class SortingTests
             Assert.AreEqual(items[i], sortType.Items[i]);
         }
     }
+
+    [TestMethod()]
+    public void ShellSortTest()
+    {
+        //Arrange
+        sortType = new ShellSort<int>();
+        sortType.FillRandom(10, items);
+
+        //Act
+        items.Sort();
+        var timer = sortType.Sort();
+        Console.WriteLine(timer);
+        //Assert
+        for (int i = 0; i < items.Count; i++)
+        {
+            Assert.AreEqual(items[i], sortType.Items[i]);
+        }
+    }
 }
